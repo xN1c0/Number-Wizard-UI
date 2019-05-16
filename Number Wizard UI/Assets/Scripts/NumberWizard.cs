149e16e1ef0dae7aa7d3e9e3e5062be2a1c45f29
xNiC0 <45330099+xNiC0@users.noreply.github.com>
2019-05-16 23:13:40 +0200

    NWUI Completed
    Tutte le funzionalita' implementate

---
+++ Number Wizard UI/Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI numberGuessd;
+    int guess;
+
+    void Start()
+    {
+        Debug.Log("Highest number is: " + max);
+        Debug.Log("Lowest number is: " + min);
+        max = max + 1;
+        guess = Random.Range(min, max);
+        numberGuessd.text = guess.ToString();
+    }
+
+    void NextGuess()
+    {
+        guess = Random.Range(min, max);
+        Debug.Log("New guess is " + guess);
+        numberGuessd.text = guess.ToString();
+    }
+
+    public void onPressHigher()
+    {
+        if(max - min > 1)
+            min = guess + 1;
+        NextGuess();
+    }
+
+    public void onPressLower()
+    {
+        if(max - min > 1)
+            max = guess - 1;
+        NextGuess();
+    }
+
+
+}
